7d42b78962d27fd2ef6878a83e6c0ab7dbad6d34
satpal82bhandari <satpal82bhandari@gmail.com>
2024-09-11 15:48:59 +0530

    puppet program cpi with pda

---
+++ programs/puppet-master/src/lib.rs
@@ -0,0 +1,42 @@
+use anchor_lang::prelude::*;
+use puppet::cpi::accounts::SetData;
+use puppet::program::Puppet;
+use puppet::{self, Data};
+
+
+declare_id!("GnirgarFXEmqoQkkTTMu8t7kjWFA4MtuzNaXAE5UwitV");
+
+
+#[program]
+mod puppet_master {
+    use super::*;
+    pub fn pull_strings(ctx: Context<PullStrings>, bump: u8, data: u64) -> Result<()> {
+        let bump = &[bump][..];
+        puppet::cpi::set_data(
+            ctx.accounts.set_data_ctx().with_signer(&[&[bump][..]]),
+            data,
+        )
+    }
+}
+
+
+#[derive(Accounts)]
+pub struct PullStrings<'info> {
+    #[account(mut)]
+    pub puppet: Account<'info, Data>,
+    pub puppet_program: Program<'info, Puppet>,
+    /// CHECK: only used as a signing PDA
+    pub authority: UncheckedAccount<'info>,
+}
+
+
+impl<'info> PullStrings<'info> {
+    pub fn set_data_ctx(&self) -> CpiContext<'_, '_, '_, 'info, SetData<'info>> {
+        let cpi_program = self.puppet_program.to_account_info();
+        let cpi_accounts = SetData {
+            puppet: self.puppet.to_account_info(),
+            authority: self.authority.to_account_info(),
+        };
+        CpiContext::new(cpi_program, cpi_accounts)
+    }
+}
---
+++ tests/puppet.ts
@@ -0,0 +1,117 @@
+import * as anchor from '@coral-xyz/anchor'
+import { Program } from '@coral-xyz/anchor'
+import { Keypair, PublicKey } from '@solana/web3.js'
+import { Puppet } from '../target/types/puppet'
+import { PuppetMaster } from '../target/types/puppet_master'
+import { expect } from 'chai'
+
+
+describe('puppet', () => {
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider)
+
+
+  const puppetProgram = anchor.workspace.Puppet as Program<Puppet>
+  const puppetMasterProgram = anchor.workspace
+    .PuppetMaster as Program<PuppetMaster>
+
+
+  const puppetKeypair = Keypair.generate();
+  console.log("***************************");
+  console.log("puppet account : ",puppetKeypair.publicKey.toBase58());
+  console.log("***************************");
+  console.log("user or wallet : ", provider.wallet.publicKey.toBase58())
+  console.log("***************************");
+
+
+  it('Does CPI!', async () => {
+    const [puppetMasterPDA, puppetMasterBump] =
+      await PublicKey.findProgramAddressSync([], puppetMasterProgram.programId);
+
+      console.log("****************************")
+      console.log("PuppetMasterPDA : ", puppetMasterPDA.toBase58())
+      console.log("****************************")
+
+      
+
+    let txn1 = await puppetProgram.methods
+      .initialize(puppetMasterPDA)
+      .accounts({
+        puppet: puppetKeypair.publicKey,
+        user: provider.wallet.publicKey,
+      })
+      .signers([puppetKeypair])
+      .rpc();
+    
+      console.log("****************************");
+      console.log("initialize transaction : ", txn1);
+      console.log("****************************");
+      //print transaction logs in cli :
+      const connection = puppetMasterProgram.provider.connection;
+      const latestBlockHash = await connection.getLatestBlockhash();
+      await connection.confirmTransaction(
+        {
+          blockhash: latestBlockHash.blockhash,
+          lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+          signature: txn1,
+        },
+        "confirmed"
+      );
+      const txDetails = await puppetMasterProgram.provider.connection.getTransaction(txn1, {
+        maxSupportedTransactionVersion: 0,
+        commitment: "confirmed",
+      });
+      console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+      console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+      console.log(txDetails);
+      console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+      console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+      console.log(txDetails.meta.logMessages)
+      console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"); 
+
+//-----------------------------------------------------------------------------------------------------
+
+    let txn2 = await puppetMasterProgram.methods
+      .pullStrings(puppetMasterBump, new anchor.BN(42))
+      .accounts({
+        puppetProgram: puppetProgram.programId,
+        puppet: puppetKeypair.publicKey,
+        authority: puppetMasterPDA,
+      })
+      .rpc();
+
+      console.log("****************************")
+      console.log("pullstring transaction : ", txn2)
+      console.log("****************************")
+      const connection2 = puppetMasterProgram.provider.connection;
+      const latestBlockHash2 = await connection2.getLatestBlockhash();
+      await connection2.confirmTransaction(
+        {
+          blockhash: latestBlockHash2.blockhash,
+          lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+          signature: txn2,
+        },
+        "confirmed"
+      );
+      const txDetails2 = await puppetMasterProgram.provider.connection.getTransaction(txn2, {
+        maxSupportedTransactionVersion: 0,
+        commitment: "confirmed",
+      });
+      console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+      console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+      console.log(txDetails2);
+      console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+      console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+      console.log(txDetails2.meta.logMessages)
+      console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+      
+
+
+    expect(
+      (
+        await puppetProgram.account.data.fetch(puppetKeypair.publicKey)
+      ).data.toNumber()
+    ).to.equal(42)
+  })
+})
